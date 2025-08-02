06819b69d8a865f491daa1c64e1fc95e1e2fcfc4
Михаил <kamaltdinova7566@gmail.com>
2025-08-02 12:13:31 +0300

    fixed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -27,12 +27,19 @@ export const Header = ({onScrollToRef,
         <div className='xs:pt-[30px] md:flex flex-row'>
             {!isMobile && <div className=''><img className='rounded-r-[30px]' width={'700'} src={sideImg} alt="nasos"/></div>}
             <div className='md:w-[1200px] md:pr-[100px]'>
-                <div className='bg-[#4CBFCC] flex flex-row h-[40px] rounded-b-[10px] items-center justify-end text-white'>
-                    <text className='pr-[10px]'><em>+7(931) 233-39-11</em></text>
-                    <img src={whatsapp} alt={'whatsapp'} className='w-[30px]'></img>
-                </div>
-                <div className='flex justify-between pl-[10px] pr-[10px] md:pr-[55px]'>
+                {isMobile &&
+                    <div className='bg-[#4CBFCC] flex flex-row h-[40px] rounded-b-[10px] items-center justify-end text-white'>
+                        <text className='pr-[10px]'><em>+7(931) 233-39-11</em></text>
+                        <img src={whatsapp} alt={'whatsapp'} className='w-[30px]'></img>
+                    </div>}
+                <div className='flex justify-between pl-[10px] md:items-center pr-[10px] md:pr-[55px]'>
                     <h1 className='text-[56px] md:text-[60px] pt-[30px] font-bold md:pl-[50px] text-[#51D1E0]'>AquaMaster</h1>
+                    {!isMobile &&
+                        <div className='flex items-center pt-[40px]'>
+                            <text className='text-[24px] pr-[10px]'><em>+7(931) 233-39-11</em></text>
+                            <img src={whatsapp} alt={'whatsapp'} className='w-[50px]'></img>
+                        </div>
+                    }
                 </div>
                 <div className='flex gap-1 justify-between pt-[20px] pl-[10px] pr-[10px] md:pl-[60px] md:pr-[60px]'>
                     <Button
@@ -49,10 +56,10 @@ export const Header = ({onScrollToRef,
                     />
                 </div>
                 <img src={line} alt={'line'} className=' pt-[29px]'/>
-                <div className='pt-[20px] pl-[50px] text-[20px] md:text-end md:text-[36px] md:pr-[20px]'>
+                <div className='pt-[20px] pl-[10px] md:pl-[50px] text-[20px] md:text-end md:text-[36px] md:pr-[20px]'>
                     <em>Мы Предлагаем полный комплекс услуг по обслуживанию и ремонту скважин на воду в Санкт-Петербурге и Ленинградской области</em>
                 </div>
-                <div className='pl-[50px]'>
+                <div className='pl-[15px] md:pl-[50px]'>
                     <div className='flex flex-col'>
                         <div className='flex flex-col pt-[40px] md:pt-[82px] gap-[10px]'>
                             <div className='flex'>
