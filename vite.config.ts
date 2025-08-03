4b82d43c103100d8f6b9b8a243711c298c867dce
Михаил <kamaltdinova7566@gmail.com>
2025-08-03 15:48:13 +0300

    deploy 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/pchelpatao-aquamaster-cd89.twc1/',
+  base: '/pchelpatao-aquamaster-cd89.twc1.net/',
   plugins: [react(), tailwindcss()],
 })
