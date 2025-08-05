4b5b8d6c6381d518df7a87096579f56d70de0a94
Михаил <kamaltdinova7566@gmail.com>
2025-08-05 07:42:21 +0300

    fixed

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -12,7 +12,7 @@ type ButtonProps = {
 export const Button = ({name, withoutIcon = false, padding, onClick, isActive = false, className
 }: ButtonProps) => {
     return (
-        <div className={` rounded-[90px] ${className} md:h-[50px] md:w-[400px] w-[120px] border-[#31919C] border-[2px] md:border-[4px] cursor-pointer bg-[#4CBFCC] flex flex-row hover:bg-[#31919C]  text-white font-bold hover: ${isActive ? 'bg-[#31919C]' : ''}`}>
+        <div className={` rounded-[90px] ${className} md:h-[50px] md:w-[400px] w-[120px] border-[#31919C] border-[1px] md:border-[2px] cursor-pointer bg-[#4CBFCC] flex flex-row hover:bg-[#31919C]  text-white font-bold hover: ${isActive ? 'bg-[#31919C]' : ''}`}>
             <button
                 style={{padding}}
                 className={`w-full flex flex-row ${withoutIcon ? 'justify-center' : ''}`}
